34c00b24c1080c8a7e528badee12d10c0c76778e
Son Tran <tson171192@gmail.com>
2019-04-22 13:24:28 +0700

    [Laravel and Vue] 22/4/2019

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('{path}','HomeController@index')->where('path','([A-z\d-\/_.]+)?' );
